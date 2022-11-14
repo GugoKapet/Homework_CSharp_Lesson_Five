70ec281f4307671c5309b472e7c32d60690c0b32
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-11-14 18:46:30 +0200

    Added Task 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] massive = new int[20];
+FillArray(massive);
+PrintArray(massive);
+Console.WriteLine();
+int evenNumbers = CountEvenNumbers(massive);
+Console.WriteLine($"Количество четных чисел массива: {evenNumbers}");
+
+void FillArray(int[] m)
+{
+    Random random = new Random();
+    for (int i = 0; i < m.Length; i++)
+    {
+        m[i] = random.Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine(massive[i]);
+    }
+}
+
+int CountEvenNumbers(int[] b)
+{
+    int count = 0;
+    for (int i = 0; i < b.Length; i++)
+    {
+        if (b[i]%2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
